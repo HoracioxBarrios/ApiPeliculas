15ef3ed5f1431691b2117c962e2de25f758313a4
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-08-06 10:26:51 -0300

    EndPoint GetCategorias ---> OK

---
+++ ApiPeliculas/Controllers/CategoriasController.cs
@@ -0,0 +1,40 @@
+﻿using ApiPeliculas.Models.DataTransferObjects;
+using ApiPeliculas.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    //[Route("api/[controller]")] opcion por defecto
+    [Route("api/categorias")] // opcion dinamica
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+
+        private readonly ICategoriaCrudRepositorio _ctRepo;
+        private readonly IMapper _mapper;
+
+        public CategoriasController(ICategoriaCrudRepositorio ctRepo, IMapper maper)
+        {
+            _ctRepo = ctRepo;
+            _mapper = maper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCayegorias()
+        {
+            var listaCategorias = _ctRepo.GetCategorias().ToList();
+
+            var listaCategoriasDto = new List<CategoriaDto>();// Vamos a exponer el Dto en lugar de la Lista de Categorias directamente
+
+            foreach(var categoria in listaCategorias)
+            {
+                listaCategoriasDto.Add(_mapper.Map<CategoriaDto>(categoria));
+            }
+            return Ok(listaCategoriasDto);
+        }
+    }
+}
--- ApiPeliculas/Program.cs
+++ ApiPeliculas/Program.cs
@@ -1,10 +1,10 @@
 using ApiPeliculas.Data;
+using ApiPeliculas.PeliculasMapper;
+using ApiPeliculas.Repositorio;
+using ApiPeliculas.Repositorio.IRepositorio;
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-
-
 
 // ---- Add services to the container.==>>>> CONFIGURAMOS LA RUTA AL STRING CONNECTION  ----
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
@@ -13,11 +13,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
 
 
 
+//Agregamos los Repositorios ------------>
+builder.Services.AddScoped<ICategoriaCrudRepositorio, CategoriaCrudRepositorio>();
+
+//Agregamos el AutoMapper ----------->
+builder.Services.AddAutoMapper(typeof(PeliculasMapperProfile)); // ponemos el tipo
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
